81827ec3586cfde07cba7dcca0ce071b085af9fb
Kitman <kitman.yiu@zerologix.com>
2020-12-09 13:33:28 +1100

    * init 2

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './HomePage.css'
 import Header from "../../component/Header/Header";
 
-function HomePage() {
-    return (
-        <React.Fragment>
-            <h1>Home</h1>
-            <Header/>
-        </React.Fragment>
-    );
+class HomePage extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Home</h1>
+                <Header/>
+            </React.Fragment>
+        );
+    }
 }
 
 export default HomePage;
