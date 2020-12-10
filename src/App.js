480ecaeca91de5f88f8f931ba8cc5bf89416f4c8
Kitman <kitman.yiu@zerologix.com>
2020-12-10 16:52:15 +1100

    *fix eslint

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import logo from './logo.svg';
 import React from "react";
 import './App.css';
+import Route, {Switch, Router} from 'react-router-dom';
 import HomePage from "./pages/HomePage/HomePage";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
-import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
 
 
 function App() {
     return (
-        <Router>
-            <Switch>
-                <Route path="/" exact component={HomePage}/>
-                <Route component={ErrorPage}/>
-            </Switch>
-        </Router>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route component={ErrorPage} />
+        </Switch>
+      </Router>
     );
 }
 
